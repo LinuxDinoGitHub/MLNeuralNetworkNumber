6927e6d1411ccfbe6db6a669c8d51fbf78abeaf7
LinuxDinoGitHub <95269147+LinuxDinoGitHub@users.noreply.github.com>
2024-07-17 11:41:43 +0800

    simple arbituary values and learning concepts

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('hello world')
+import numpy as np
+inputs = [1, 2, 3, 2.5]
+weights = [[0.4, 0.5, -0.2, 0.1],
+          [0.2, -0.4, 0.6, 0.9],
+           [-0.1, -0.6, 0.4, 0.1]]
+bias = 2
+
+output = np.dot(weights, inputs)
+print(output)
