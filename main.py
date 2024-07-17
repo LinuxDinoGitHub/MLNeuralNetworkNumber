cf845b8a5f56d34ce421f36be83789f5c22f9372
LinuxDinoGitHub <95269147+LinuxDinoGitHub@users.noreply.github.com>
2024-07-17 18:11:39 +0800

    added back propagation

--- main.py
+++ main.py
@@ -6,16 +6,18 @@ data = pd.read_csv('./mnist_train.csv/mnist_train.csv')
 data.head()
 data = np.array(data)
 
-row, col = data.shape
+m, n = data.shape #row length (784/785) and column length (5)
 np.random.shuffle(data)
 
 data_dev = data[0:1000].T
 Y_dev = data_dev[0]
-X_dev = data_dev[1:col]
+X_dev = data_dev[1:n]
 
-data_train = data[1000:row].T
-Y_train = data_train[0]
-Y_train = data_train[1:col]
+data_train = data[1000:m].T
+X_train = data_train[0]
+Y_train = data_train[1:n]
+
+print(X_train[0].shape)
 
 def init_params():
     W1 = np.random.randn(10, 784) - 0.5
@@ -28,5 +30,27 @@ def ReLU(Z):
 def softMax(Z):
     return np.exp(Z) / np.sum(np.exp(Z))
 def forward(W1, b1, W2, b2, X):
-    pass
-    
+    Z1 = W1.dot(X) + b1
+    A1 = ReLU(Z1)
+    Z2 = W2.dot(A1) + b2
+    A2 = softMax(A1)
+    return Z1, A1, Z2, A2
+def one_hot(Y):
+    one_hot_Y = np.zeros((Y.size, Y.max() + 1))
+    one_hot_Y[np.arange(Y.size), Y] = 1
+    one_hot_Y = one_hot_Y.T
+    return one_hot_Y
+def deriv_ReLU(Z):
+    return Z > 0
+def back_prop(Z1, A1, Z2, A2, W2, X, Y):
+    m = Y.size
+    one_hot_Y = one_hot(Y)
+    dZ2 = A2 - one_hot_Y
+    dW2 = 1 / m * dZ2.dot(A1.T)
+    db2 = 1 / m * np.sum(dZ2, 2)
+    dZ1 = W2.T.dot(dZ2) * deriv_ReLU(Z1)
+    dW1 = 1 / m * dZ1.dot(X.T)
+    db1 = 1 / m * np.sum(dZ1, 2)
+    return dW1, db1, dW2, db2
+
+
