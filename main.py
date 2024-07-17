cd34dbfd008ddbc2da4c3883d75c538f77d98c5a
LinuxDinoGitHub <95269147+LinuxDinoGitHub@users.noreply.github.com>
2024-07-17 12:06:44 +0800

    Layer object created

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import numpy as np
-inputs = [1, 2, 3, 2.5]
-weights = [[0.4, 0.5, -0.2, 0.1],
-          [0.2, -0.4, 0.6, 0.9],
-           [-0.1, -0.6, 0.4, 0.1]]
-bias = 2
 
-output = np.dot(weights, inputs)
-print(output)
+np.random.seed(0)
+X = [[1, 2, 3, 2.5],
+    [2, 6, 5, -3],
+    [3.3, 2.6, -1.9, 2.2]]
+
+class Layer:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.1*np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+layer1 = Layer(4,3)
+layer2 = Layer(3,2)
+
+layer1.forward(X)
+print(layer1.output)
+layer2.forward(layer1.output)
+print(layer2.output)
